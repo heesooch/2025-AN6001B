0e0c593a688972c884d1dd6de2b3869947478fa0
Heesoo Choi <heeschoi98@gmail.com>
2025-11-21 14:20:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/main", methods=["GET", "POST"])
+
 def index():
     return(render_template("index.html"))
 
+@app.route("/main
+
+def main():
+    return(render_template("main.html"))
+    q = request.form.get("q")
+    print(q)
+    
 if __name__ == "__main__":
     app.run()
